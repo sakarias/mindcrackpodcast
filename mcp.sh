bf2b5d8fd5f807f39385d4bfb92988c93692692f
John Johannessen <johnj@slaskdot.org>
2013-05-30 14:43:26 +0200

    Alpha version of the script that download and converts

---
+++ mcp.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# brew install ffmpeg
+# brew install youtube-dl
+# brew install atomicparsley
+
+TMP="/tmp/mindcrackpodcast"
+
+if [ ! -d ${TMP} ]; then
+	mkdir ${TMP}
+fi
+
+cd ${TMP}
+
+mcpUrl=$(curl -s -q https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=mindcracklp | grep 'Podcast' | head -n 1 | grep -Eo 'http://[[:alnum:][:punct:]]*[[:alnum:]]')
+
+youtube-dl -x ${mcpUrl}
+
+for file in *.m4a
+do
+	title=$(echo ${file} | cut -d "-" -f 1 | sed -e 's/^ *//g' -e 's/ *$//g')
+	episode=$(echo ${file} | cut -d "-" -f 2 | sed -e 's/^ *//g' -e 's/ *$//g')
+	episodeNumber=$(echo ${episode} | cut -d " " -f 2 | sed -e 's/^ *//g' -e 's/ *$//g')
+	extra=$(echo ${file} | cut -d "-" -f 3 | sed -e 's/^ *//g' -e 's/ *$//g')
+
+	AtomicParsley "${file}" --artist "MindCrack" --title "${title} - ${episode} ${extra}" --tracknum ${episodeNumber} --podcastFlag true
+
+	rm "${file}"
+done
+
+mv *temp* "$(echo -e "${title} - ${episode} - ${extra}.m4a")"
