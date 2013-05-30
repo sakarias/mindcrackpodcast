24e420bea2cc7752c493d1c6b3daa0e719783d9d
John Johannessen <johnj@slaskdot.org>
2013-05-31 00:19:22 +0200

    Added a check if the podcast is downloaded or not

--- mcp.sh
+++ mcp.sh
@@ -4,15 +4,28 @@
 # brew install youtube-dl
 # brew install atomicparsley
 
+## TODO:
+# Sjekke om $extra er tom eller ikke
+
 TMP="/tmp/mindcrackpodcast"
 
 if [ ! -d ${TMP} ]; then
 	mkdir ${TMP}
 fi
 
-cd ${TMP}
-
 mcpUrl=$(curl -s -q https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=mindcracklp | grep 'Podcast' | head -n 1 | grep -Eo 'http://[[:alnum:][:punct:]]*[[:alnum:]]')
+
+if [ -f lastDownloaded ]; then
+	lastDownloaded=$(cat lastDownloaded)
+	if [ "${mcpUrl}" == "${lastDownloaded}" ]; then
+		echo "Already downloaded this episode."
+		exit 0
+	fi
+else
+	echo ${mcpUrl} > lastDownloaded
+fi
+
+cd ${TMP}
 
 youtube-dl -x ${mcpUrl}
 
@@ -26,6 +39,8 @@ do
 	AtomicParsley "${file}" --artist "MindCrack" --album "MindCrack" --title "${title} - ${episode} - ${extra}" --tracknum ${episodeNumber} --podcastFlag true
 
 	rm "${file}"
+
+	mv *temp* "$(echo -e "${title} - ${episode} - ${extra}.m4a")"
 done
 
-mv *temp* "$(echo -e "${title} - ${episode} - ${extra}.m4a")"
+
